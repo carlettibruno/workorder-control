e28401b14c83cd683a388fb851c873e8757004ac
Bruno Carletti <carletti.bruno@gmail.com>
2018-12-14 21:17:41 -0200

    changing login method

--- controle-thermalprinter/src/br/com/carlettisolucoes/thermalprinter/service/ServiceRequest.java
+++ controle-thermalprinter/src/br/com/carlettisolucoes/thermalprinter/service/ServiceRequest.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.csi.controle.core.entidade.FilaImpressora;
-import org.csi.controle.core.to.Login;
 import org.csi.controle.core.util.RetornoServico;
 
 import com.google.gson.Gson;
@@ -23,52 +22,55 @@ import br.com.carlettisolucoes.thermalprinter.util.Util;
 public class ServiceRequest {
 
 	private static ServiceRequest instance;
-	
+
 	private String token;
-	
+
 	private String urlInfoPrint;
-	
-	private String urlLogin;
-	
+
+//	private String urlLogin;
+
+//	private ServiceRequest() {
+//	}
+
+//	private ServiceRequest(String user, String password) {
 	private ServiceRequest() {
-	}
-	
-	private ServiceRequest(String user, String password) {
 		try {
 			urlInfoPrint = Util.getValor("service.getinfoprint.url");
-			urlLogin = Util.getValor("service.url");
-			token = gerarToken(user, password);
+//			urlLogin = Util.getValor("service.url");
+			//token = gerarToken(user, password);
+			token = "bcc8088ae11d78ac9b96a67b0423a07c";
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static ServiceRequest getInstance() {
 		if(instance == null) {
-			try {
-				String user = Util.getValor("service.user");
-				String password = Util.getValor("service.password");
-				instance = new ServiceRequest(user, password);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+//			try {
+//				String user = Util.getValor("service.user");
+//				String password = Util.getValor("service.password");
+//				instance = new ServiceRequest(user, password);
+				instance = new ServiceRequest();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
 		}
 		return instance;
 	}
-	
-	@SuppressWarnings("unchecked")
-	private String gerarToken(String usuario, String senha) throws IOException {
-		Login login = new Login();
-		login.setUsuario(usuario);
-		login.setSenha(senha);
-		login.setManterConectado(true);
-		Gson gson = new Gson();
-		String jsonRequest = gson.toJson(login);
-		String json = request(urlLogin, "POST", "", jsonRequest);
-		RetornoServico<String> retorno = gson.fromJson(json, RetornoServico.class);
-		return retorno.getData();
-	}
-	
+
+//	@SuppressWarnings("unchecked")
+//	private String gerarToken(String usuario, String senha) throws IOException {
+//		Login login = new Login();
+//		login.setUsuario(usuario);
+//		login.setSenha(senha);
+//		login.setManterConectado(true);
+//		Gson gson = new Gson();
+//		String jsonRequest = gson.toJson(login);
+//		String json = request(urlLogin, "POST", "", jsonRequest);
+//		RetornoServico<String> retorno = gson.fromJson(json, RetornoServico.class);
+//		return retorno.getData();
+//	}
+
 	@SuppressWarnings("unchecked")
 	public List<FilaImpressora> getListInfoPrint() {
 		try {
@@ -84,10 +86,10 @@ public class ServiceRequest {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return new ArrayList<FilaImpressora>();
 	}
-	
+
 	public void removeFilaImpressora(FilaImpressora filaImpressora) {
 		try {
 			request(urlInfoPrint + "?fiid="+filaImpressora.getIdFilaImpressora(), "DELETE", token, null);
@@ -95,7 +97,7 @@ public class ServiceRequest {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private String request(String urlStr, String method, String token, String jsonPost) throws IOException {
 		URL url = new URL(urlStr);
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -106,7 +108,7 @@ public class ServiceRequest {
 		if(!method.equals("DELETE")) {
 			connection.setDoOutput(true);
 		}
-		
+
 		if(jsonPost != null) {
 		    connection.setRequestProperty("Content-Type", "application/json");
 			DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
@@ -114,7 +116,7 @@ public class ServiceRequest {
 			wr.flush ();
 			wr.close ();
 		}
-		
+
 		BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream(),Charset.forName("UTF-8")));
 		String response = "";
 		String line = "";
@@ -123,6 +125,6 @@ public class ServiceRequest {
 		}
 		rd.close();
 		return response;
-	}	
-	
+	}
+
 }
