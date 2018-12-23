182762d092d1eea734819ab86d8573a4354258d2
Bruno Carletti <carletti.bruno@gmail.com>
2018-12-23 20:01:04 -0200

    Changing the tracking with Correios company.

--- RastreamentoCorreios/src/org/csi/rastreamento/correios/manager/CorreiosRastreio.java
+++ RastreamentoCorreios/src/org/csi/rastreamento/correios/manager/CorreiosRastreio.java
@@ -1,16 +1,17 @@
 package org.csi.rastreamento.correios.manager;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.rmi.RemoteException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.csi.rastreamento.correios.entidade.Evento;
-
-import br.com.correios.webservice.resource.Eventos;
-import br.com.correios.webservice.resource.Objeto;
-import br.com.correios.webservice.resource.ServiceProxy;
-import br.com.correios.webservice.resource.Sroxml;
 
 public class CorreiosRastreio implements Rastreio {
 
@@ -24,24 +25,80 @@ public class CorreiosRastreio implements Rastreio {
 		}
 		return eventos;
 	}
-	
-	private List<Evento> obterEventosWsdl(String codigo, String user, String password, String wsdl) throws RemoteException {
-		ServiceProxy service = new ServiceProxy(wsdl);
-		Sroxml sroxml = service.buscaEventos(user, password, "L", "T", "101", codigo.toUpperCase());
-		Objeto obj = sroxml.getObjeto(0);
-		
+
+	private List<Evento> obterEventosWsdl(String codigo, String user, String password, String wsdl) throws IOException {
+		wsdl = wsdl.replace("#{CODE}", codigo);
+//		wsdl = "https://websro.com.br/";
+		URL url = new URL(wsdl);
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setConnectTimeout(2000);
+		con.setReadTimeout(2000);	
+		con.setRequestMethod("GET");
+		con.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+		con.setRequestProperty("Host", "www.websro.com.br");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.2 Safari/605.1.15");
+		con.setRequestProperty("Accept-Language", "en-us");
+		con.setRequestProperty("Accept-Encoding", "br, gzip, deflate");
+		con.setRequestProperty("Connection", "keep-alive");
+//		con.setRequestProperty("Cookie", "_ga=GA1.3.627365304.1545400010; _gat=1; _gid=GA1.3.115962214.1545400014; PHPSESSID=1q5vj79sr5goh6f7ipua803ht4");
+		con.setDoOutput(true);
+		DataOutputStream out = new DataOutputStream(con.getOutputStream());
+		out.flush();
+		out.close();
+
+		int status = con.getResponseCode();
 		List<Evento> eventosRet = new ArrayList<>();
-		Eventos[] eventos = obj.getEvento();
-		if(eventos == null) {
-			throw new IllegalArgumentException("FAILED TO GET EVENTS: "+obj.getErro());
+		if(status == 200) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer content = new StringBuffer();
+			while ((inputLine = in.readLine()) != null) {
+				content.append(inputLine);
+			}
+			in.close();
+//			System.out.println(content);
+			String tbody = content.substring(content.indexOf("<tbody>"), content.indexOf("</tbody>"));
+			String[] linhas = tbody.split("<tr>");
+			Pattern pattern = Pattern.compile("<td.*?>(.*)<\\/td>");
+			for (int i = 1; i < linhas.length; i++) {
+				String[] colunas = linhas[i].split("<td>");
+				String coltitulo = colunas[0].replaceAll("\t", "").replaceAll("\r", "").replaceAll("\n", "");
+				Matcher mt = pattern.matcher(coltitulo);
+				String titulo = "";
+				if(mt.find()) {
+					titulo = mt.group(1);
+					titulo = titulo.replaceAll("<br/?>", " - ").replaceAll("</?label>", "").replaceAll("</?strong>", "");
+				}
+				
+				String coldescricao = "<td>"+colunas[1].replaceAll("\t", "").replaceAll("\r", "").replaceAll("\n", "");
+				mt = pattern.matcher(coldescricao);
+				String descricao = "";
+				if(mt.find()) {
+					descricao = mt.group(1);
+					descricao = descricao.replaceAll("<br/?>", " - ").replaceAll("</?label>", "").replaceAll("</?strong>", "");
+				}
+				eventosRet.add(new Evento(titulo, descricao));
+			}
+		} else {
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer content = new StringBuffer();
+			while ((inputLine = in.readLine()) != null) {
+				content.append(inputLine);
+			}
+			in.close();
+			System.out.println(content);
 		}
-		for (Eventos evento : eventos) {
-			Evento e = new Evento();
-			e.setTitulo(evento.getLocal());
-			e.setDescricao(evento.getDescricao());
-			eventosRet.add(e);
-		}
+		
 		return eventosRet;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			new CorreiosRastreio().obterEventosWsdl("AA123456789BR", null, null, "https://websro.com.br/detalhes.php?P_COD_UNI=#{CODE}");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
